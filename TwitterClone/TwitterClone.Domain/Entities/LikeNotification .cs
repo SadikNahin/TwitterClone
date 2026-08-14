4be8db20d9b15865ba7c420d70a7c96e5d045482
DESKTOP-7VO3PEB\NAHIN <=sadiknahin1@gmail.com>
2026-08-14 16:07:51 +0600

    added likeNotification class

---
+++ TwitterClone/TwitterClone.Domain/Entities/LikeNotification .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class LikeNotification : Notification
+    {
+        public LikeNotification() : base("Like")
+        {
+        }
+
+        public Guid LikedByUserId { get; set; }
+
+        public void AddMessage(string message)
+        {
+            Message = message;
+        }
+    }
+}
